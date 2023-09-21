5ca8ea2ec455b09fb77455cc6f2fd3a771dc9a39
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-09-21 14:11:00 +0530

    implemented basic action

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("From index.js")
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+//Action Creator is a fn that returns the action
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+};
