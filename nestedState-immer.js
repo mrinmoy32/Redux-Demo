7b2efb2bb4e827bc0274484283434cf0f7fddb5b
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-09-22 09:18:46 +0530

    installed immer and imported produce method

---
+++ nestedState-immer.js
@@ -0,0 +1,51 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const produce = require('immer').produce;
+
+//Working with nested states
+const initialState = {
+  name: "Mrinmoy",
+  address: {
+    street: "123 main St",
+    city: "Boston",
+    state: "MA",
+  },
+};
+
+//Action type
+const STREET_UPDATED = "STREET_UPDATED";
+
+//Action creator
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  };
+};
+
+//reducer
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          street: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+
+store.dispatch(updateStreet("456 main St"));
+
+unsubscribe();
