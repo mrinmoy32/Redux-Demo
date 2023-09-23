f5bbaacdc32cac8b27899f30a7cb5b2c3710354d
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-09-23 13:50:07 +0530

    redux-logger middleware

---
+++ middleware-redux-logger.js
@@ -0,0 +1,134 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+
+const reduxLogger = require('redux-logger');
+// logger middleware
+const logger = reduxLogger.createLogger();
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+
+//Action Creator is a fn that returns the action
+//In React the covention use to use property "payload" for any additional inforamtion you want to send.
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  };
+};
+
+//Another action creator
+const restockCake = (qty = 1) => {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+};
+
+const orderIceCream = (qty = 1) => {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+};
+
+const restockIceCream = (qty = 1) => {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+};
+
+// State
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfpastries: 25,
+//   numOfIceCream: 12,
+//   numOfbreads: 30,
+// };
+
+// creating separate state for separate property
+const initialCakeState = {
+  numOfCakes: 10,
+};
+const initialIceCreamState = {
+  numOfIceCream: 12,
+};
+
+// (previousState, action) => newState
+//in the long run for too many action types we should create multiple reducers
+//Creating separate reducers
+//When app grows in size split the reducers and keep them in separate files
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+//combineReducers
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+
+//lets create the store, it takes reducer as parameter which in turn has initiaState
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+console.log("initial state", store.getState());
+
+// commeting console.log as we now have logger middleware 
+const unsubscribe = store.subscribe(() => {}
+  // console.log("updated state", store.getState())
+);
+
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(5));
+
+// bindActionCreators was useful back in the days, not now
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIceCream, restockIceCream },
+  store.dispatch
+);
+
+actions.orderCake();
+actions.orderIceCream();
+actions.orderCake();
+actions.restockIceCream(5);
+actions.restockCake();
+
+unsubscribe();
