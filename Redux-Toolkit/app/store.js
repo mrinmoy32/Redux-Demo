9e4f4a5ee7450204e2d6ebc4df8b36deba1f525b
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-09-26 22:33:36 +0530

    using async thunk (fetchUsers) with RTK

--- Redux-Toolkit/app/store.js
+++ Redux-Toolkit/app/store.js
@@ -1,6 +1,7 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/cake/cakeSlice');
-const iceCreamReducer = require('../features/iceCream/iceCreamSlice')
+const iceCreamReducer = require('../features/iceCream/iceCreamSlice');
+const userReducer = require('../features/user/userSlice')
 
 const reduxLogger = require('redux-logger');
 // logger middleware
@@ -9,7 +10,8 @@ const logger = reduxLogger.createLogger();
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
-        iceCream: iceCreamReducer
+        iceCream: iceCreamReducer,
+        user: userReducer
     },
     // in the list of default middlewares created by configureStore we append our logger middleware
     // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),  
--- Redux-Toolkit/index.js
+++ Redux-Toolkit/index.js
@@ -1,20 +1,22 @@
 const store  = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const iceCreamActions = require('./features/iceCream/iceCreamSlice').iceCreamActions;
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
 
 console.log('initial state', store.getState());
 const unsubscribe = store.subscribe(() => {
     console.log('updated state', store.getState())
 })
 
+store.dispatch(fetchUsers());
 store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(5))
 
 store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.restocked(3))
 
-
-unsubscribe();
+// unsubscribe can not be used with async func
+// unsubscribe();
