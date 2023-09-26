221c444e896853a5bc9c897c369e680f533a404d
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-09-27 00:29:51 +0530

    created a react rtk app using "vite"

---
+++ react-redux-toolkit/src/app/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cakeReducer from '../features/cake/cakeSlice';
+import iceCreamReducer from '../features/iceCream/iceCreamSlice';
+import userReducer from '../features/user/userSlice';
+
+// import { createLogger } from 'redux-logger';
+// logger middleware
+// const logger = createLogger(); 
+
+const store = configureStore({
+    reducer: {
+        cake: cakeReducer,
+        iceCream: iceCreamReducer,
+        user: userReducer
+    },
+    // in the list of default middlewares created by configureStore we append our logger middleware
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),  
+})
+
+export default store;
