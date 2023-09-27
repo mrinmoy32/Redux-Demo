ab64411f305f8809474be1c4bec9cb789a962843
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-09-27 21:42:30 +0530

    dispatching actions: useDispatch from react-redux

--- react-redux-toolkit/src/features/cake/CakeView.jsx
+++ react-redux-toolkit/src/features/cake/CakeView.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './cakeSlice';
 
 export const CakeView = () => {
+    const [restockValue, setRestockValue] = useState(1)
     // To read data from redux store in a react component we use useSelector hook from react-redux library
     // useSelector returns whatever is returned by its callback (selector)  function
     const numOfCakes  = useSelector((state) => state.cake.numOfCakes);
-    console.log(numOfCakes)
+    // useDispatch is used to dispatch action with react-redux
+    const dispatch = useDispatch()
   return (
     <div>
-        <h2>Number of cakes - {numOfCakes}</h2>
-        <button>Order Cake</button>
-        <button>Restock Cakes</button>
+        <h2>Number of cakes : {numOfCakes}</h2>
+        <button onClick={() => {dispatch(ordered())}}>Order Cake</button>
+        {/* only store those states in redux store  which are required outside of that component
+        Some state can be just internal like the restockValue and not to be stored in Redux-Store(to not make store complicated) */}
+        <input type='number' onChange={e => setRestockValue(parseInt(e.target.value))}/>
+        <button onClick={() => {dispatch(restocked(restockValue))}}>Restock Cakes</button>
     </div>
   )
 }
--- react-redux-toolkit/src/features/iceCream/IceCreamView.jsx
+++ react-redux-toolkit/src/features/iceCream/IceCreamView.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { ordered, restocked } from './iceCreamSlice';
 
 export const IceCreamView = () => {
     // To read data from redux store in a react component we use useSelector hook from react-redux library
     // useSelector returns whatever is returned by its callback (selector)  function
-    const numOfIceCreams  = useSelector((state) => state.iceCream.numOfIceCreams)
+    const numOfIceCreams  = useSelector((state) => state.iceCream.numOfIceCreams);
+    const dispatch = useDispatch();
+    const HandleIceCreamOrder = () => dispatch(ordered());
+    const HandleIceCreamRestock = () => dispatch(restocked(5));
   return (
     <div>
-        <h2>Number of ice creams - {numOfIceCreams}</h2>
-        <button>Order ice cream</button>
-        <button>Restock ice creams</button>
+        <h2>Number of ice creams : {numOfIceCreams}</h2>
+        <button onClick={HandleIceCreamOrder}>Order ice cream</button>
+        <button onClick={HandleIceCreamRestock}>Restock ice creams</button>
     </div>
   )
 }
