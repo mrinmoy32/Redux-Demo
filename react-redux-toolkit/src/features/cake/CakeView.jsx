aa9e5df443929e7c46cb2936b69eacdfd8a1889c
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-09-27 20:01:46 +0530

    useSelector to read data from redux store

--- react-redux-toolkit/src/features/cake/CakeView.jsx
+++ react-redux-toolkit/src/features/cake/CakeView.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const CakeView = () => {
+    // To read data from redux store in a react component we use useSelector hook from react-redux library
+    // useSelector returns whatever is returned by its callback (selector)  function
+    const numOfCakes  = useSelector((state) => state.cake.numOfCakes);
+    console.log(numOfCakes)
   return (
     <div>
-        <h2>Number of cakes - </h2>
+        <h2>Number of cakes - {numOfCakes}</h2>
         <button>Order Cake</button>
         <button>Restock Cakes</button>
     </div>
--- react-redux-toolkit/src/features/iceCream/IceCreamView.jsx
+++ react-redux-toolkit/src/features/iceCream/IceCreamView.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux'
 
 export const IceCreamView = () => {
+    // To read data from redux store in a react component we use useSelector hook from react-redux library
+    // useSelector returns whatever is returned by its callback (selector)  function
+    const numOfIceCreams  = useSelector((state) => state.iceCream.numOfIceCreams)
   return (
     <div>
-        <h2>Number of ice creams - </h2>
+        <h2>Number of ice creams - {numOfIceCreams}</h2>
         <button>Order ice cream</button>
         <button>Restock ice creams</button>
     </div>
